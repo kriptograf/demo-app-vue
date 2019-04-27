0d0b41279cbcd6f5ed977bfb6538f8d1489bdd77
foreach <foreach@mail.ru>
2019-04-28 00:25:26 +0300

    add login page add register page create main page

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,9 @@
 
     <v-toolbar app dark color="primary">
       <v-toolbar-side-icon @click="drawer = !drawer" class="hidden-md-and-up"></v-toolbar-side-icon>
-      <v-toolbar-title>Ads</v-toolbar-title>
+      <v-toolbar-title>
+        <router-link to="/" tag="span" class="pointer">Ads</router-link>
+      </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-toolbar-items class="hidden-sm-and-down">
         <v-btn flat v-for="link of links" :key="link.title" :to="link.url">
@@ -56,3 +58,9 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+  .pointer{
+    cursor: pointer;
+  }
+</style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,16 +1,76 @@
 <template>
-    <v-container>
-        <v-layout row>
-            <v-flex xs12>
-                <h1>Home</h1>
-            </v-flex>
-        </v-layout>
-    </v-container>
+    <div>
+        <v-container fluid>
+            <v-layout row>
+                <v-flex xs12>
+                    <v-carousel>
+                        <v-carousel-item
+                                v-for="(item,i) in items"
+                                :key="i"
+                                :src="item.src"
+                        ></v-carousel-item>
+                    </v-carousel>
+                </v-flex>
+            </v-layout>
+        </v-container>
+        <v-container grid-list-lg>
+            <v-layout row wrap>
+                <v-flex xs12 sm6 md4 v-for="item of items" :key="item.id">
+                    <v-card>
+                        <v-img
+                                :src="item.src"
+                                aspect-ratio="2.75"
+                        ></v-img>
+
+                        <v-card-title primary-title>
+                            <div>
+                                <h3 class="headline mb-0">{{ item.title }}</h3>
+                                <div> {{ item.description }} </div>
+                            </div>
+                        </v-card-title>
+
+                        <v-card-actions>
+                            <v-btn flat color="orange">Share</v-btn>
+                            <v-btn flat color="orange">Explore</v-btn>
+                        </v-card-actions>
+                    </v-card>
+                </v-flex>
+            </v-layout>
+        </v-container>
+    </div>
+
 </template>
 
 <script>
     export default {
-        name: "Home"
+        name: "Home",
+        data(){
+            return {
+                items: [
+                    {
+                        title: 'Lorem ipsum dolor sit amet',
+                        description: 'Lorem ipsum dolor sit amet',
+                        promo: false,
+                        src: require('./../assets/images/card-thumb-1.jpg'),
+                        id: '1'
+                    },
+                    {
+                        title: 'rewe',
+                        description: 'dfgsdfg',
+                        promo: false,
+                        src: require('./../assets/images/card-thumb-2.jpg'),
+                        id: '1'
+                    },
+                    {
+                        title: 'sdasd',
+                        description: 'xzxczxc',
+                        promo: false,
+                        src: require('./../assets/images/card-thumb-3.jpg'),
+                        id: '1'
+                    }
+                ]
+            }
+        }
     }
 </script>
 
