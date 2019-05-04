633ac019212333f011edd2c7be5e9e9a263bb1db
foreach <foreach@mail.ru>
2019-05-04 15:11:33 +0300

    add detail page ad

--- src/components/AdsDetail.vue
+++ src/components/AdsDetail.vue
@@ -2,7 +2,18 @@
     <v-container>
         <v-layout row>
             <v-flex xs12>
-                <h1>Detail Ads</h1>
+                <v-card>
+                    <v-card-media src="https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg" height="300px"></v-card-media>
+                    <v-card-text>
+                        <h1 class="text--primary">Lorem ipsum dolor sit amet.</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At cumque est et ex molestias, quis.</p>
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn class="warning">Edit</v-btn>
+                        <v-btn class="success">Buy</v-btn>
+                    </v-card-actions>
+                </v-card>
             </v-flex>
         </v-layout>
     </v-container>
